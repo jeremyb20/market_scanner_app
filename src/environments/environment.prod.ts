2cc2c519b765b91b1dc5c262feb0a38951d0cc78
Jeremy <jbaccab@ucenfotec.ac.cr>
2022-05-05 20:42:10 -0600

    check api post

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   environmentName: 'prod',
-  ws: 'https://super-market-qr-scanner.herokuapp.com'
+  ws: 'https://super-market-qr-scanner.herokuapp.com/api'
 }
